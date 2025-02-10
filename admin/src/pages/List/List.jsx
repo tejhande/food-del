7ec902140d1778cca6ec8a15a5b23ddc95fd6135
tejhande <tejashande143143@gmail.com>
2025-02-10 16:29:33 +0530

    Rename admin panel title and enhance food list styling with delete button

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,44 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import './List.css'
-import { url, currency } from '../../assets/assets'
-import axios from 'axios';
-import { toast } from 'react-toastify';
+import React, { useEffect, useState } from "react";
+import "./List.css";
+import { url, currency } from "../../assets/assets";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const List = () => {
-
   const [list, setList] = useState([]);
 
   const fetchList = async () => {
-    const response = await axios.get(`${url}/api/food/list`)
+    const response = await axios.get(`${url}/api/food/list`);
     if (response.data.success) {
       setList(response.data.data);
+    } else {
+      toast.error("Error");
     }
-    else {
-      toast.error("Error")
-    }
-  }
+  };
 
   const removeFood = async (foodId) => {
     const response = await axios.post(`${url}/api/food/remove`, {
-      id: foodId
-    })
+      id: foodId,
+    });
     await fetchList();
     if (response.data.success) {
       toast.success(response.data.message);
+    } else {
+      toast.error("Error");
     }
-    else {
-      toast.error("Error")
-    }
-  }
+  };
 
   useEffect(() => {
     fetchList();
-  }, [])
+  }, []);
 
   return (
-    <div className='list add flex-col'>
+    <div className="list add flex-col">
       <p>All Foods List</p>
-      <div className='list-table'>
+      <div className="list-table">
         <div className="list-table-format title">
           <b>Image</b>
           <b>Name</b>
@@ -48,18 +45,36 @@ const List = () => {
         </div>
         {list.map((item, index) => {
           return (
-            <div key={index} className='list-table-format'>
+            <div key={index} className="list-table-format">
               <img src={`${url}/images/` + item.image} alt="" />
               <p>{item.name}</p>
               <p>{item.category}</p>
-              <p>{currency}{item.price}</p>
-              <p className='cursor' onClick={() => removeFood(item._id)}>x</p>
+              <p>
+                {currency}
+                {item.price}
+              </p>
+              <p
+                className="delete-button noselect cursor"
+                onClick={() => removeFood(item._id)}
+              >
+                <span className="text">Delete</span>
+                <span className="icon">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path>
+                  </svg>
+                </span>
+              </p>
             </div>
-          )
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
