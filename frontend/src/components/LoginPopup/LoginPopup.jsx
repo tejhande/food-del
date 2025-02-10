9f8645d44026c2736df8c7cd1a9d95b3ed31d21a
tejhande <tejashande143143@gmail.com>
2025-02-10 18:27:17 +0530

    `Added styles for clickable text and checkbox alignment in LoginPopup component`

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,88 +1,120 @@
-import React, { useContext, useState } from 'react'
-import './LoginPopup.css'
-import { assets } from '../../assets/assets'
-import { StoreContext } from '../../Context/StoreContext'
-import axios from 'axios'
-import { toast } from 'react-toastify'
+import React, { useContext, useState } from "react";
+import "./LoginPopup.css";
+import { assets } from "../../assets/assets";
+import { StoreContext } from "../../Context/StoreContext";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const LoginPopup = ({ setShowLogin }) => {
+  const { setToken, url, loadCartData } = useContext(StoreContext);
+  const [currState, setCurrState] = useState("Sign Up");
 
-    const { setToken, url, loadCartData } = useContext(StoreContext)
-    const [currState, setCurrState] = useState("Sign Up");
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-    const [data, setData] = useState({
-        name: "",
-        email: "",
-        password: ""
-    })
+  // State for toggling password visibility
+  const [showPassword, setShowPassword] = useState(false);
 
-    // State for toggling password visibility
-    const [showPassword, setShowPassword] = useState(false);
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
 
-    const onChangeHandler = (event) => {
-        const name = event.target.name
-        const value = event.target.value
-        setData(data => ({ ...data, [name]: value }))
+  const onLogin = async (e) => {
+    e.preventDefault();
+
+    let new_url = url;
+    if (currState === "Login") {
+      new_url += "/api/user/login";
+    } else {
+      new_url += "/api/user/register";
     }
-
-    const onLogin = async (e) => {
-        e.preventDefault()
-
-        let new_url = url;
-        if (currState === "Login") {
-            new_url += "/api/user/login";
-        }
-        else {
-            new_url += "/api/user/register"
-        }
-        const response = await axios.post(new_url, data);
-        if (response.data.success) {
-            setToken(response.data.token)
-            localStorage.setItem("token", response.data.token)
-            loadCartData({ token: response.data.token })
-            setShowLogin(false)
-        }
-        else {
-            toast.error(response.data.message)
-        }
+    const response = await axios.post(new_url, data);
+    if (response.data.success) {
+      setToken(response.data.token);
+      localStorage.setItem("token", response.data.token);
+      loadCartData({ token: response.data.token });
+      setShowLogin(false);
+    } else {
+      toast.error(response.data.message);
     }
+  };
 
-    return (
-        <div className='login-popup'>
-            <form onSubmit={onLogin} className="login-popup-container">
-                <div className="login-popup-title">
-                    <h2>{currState}</h2> <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
-                </div>
-                <div className="login-popup-inputs">
-                    {currState === "Sign Up" ? <input name='name' onChange={onChangeHandler} value={data.name} type="text" placeholder='Enter Your Name' required /> : <></>}
-                    <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='Enter Your Email' />
-
-                    <div className="password-field">
-                        <input
-                            name='password'
-                            onChange={onChangeHandler}
-                            value={data.password}
-                            type={showPassword ? 'text' : 'password'}
-                            placeholder='Enter Password'
-                            required
-                        />
-                        <span className="eye-icon" onClick={() => setShowPassword(!showPassword)}>
-                            {showPassword ? '👁‍🗨' : '👁️'}
-                        </span>
-                    </div>
-                </div>
-                <button>{currState === "Login" ? "Login" : "Create account"}</button>
-                <div className="login-popup-condition">
-                    <input type="checkbox" name="" id="" required />
-                    <p>By continuing, I agree to the terms of use & privacy policy.</p>
-                </div>
-                {currState === "Login"
-                    ? <p>Create a new account? <span onClick={() => setCurrState('Sign Up')}>Click here</span></p>
-                    : <p>Already have an account? <span onClick={() => setCurrState('Login')}>Login here</span></p>
-                }
-            </form>
+  return (
+    <div className="login-popup">
+      <form onSubmit={onLogin} className="login-popup-container">
+        <div className="login-popup-title">
+          <h2>{currState}</h2>{" "}
+          <img
+            onClick={() => setShowLogin(false)}
+            src={assets.cross_icon}
+            alt=""
+          />
         </div>
-    )
-}
+        <div className="login-popup-inputs">
+          {currState === "Sign Up" ? (
+            <input
+              name="name"
+              onChange={onChangeHandler}
+              value={data.name}
+              type="text"
+              placeholder="Enter Your Name"
+              required
+            />
+          ) : (
+            <></>
+          )}
+          <input
+            name="email"
+            onChange={onChangeHandler}
+            value={data.email}
+            type="email"
+            placeholder="Enter Your Email"
+          />
 
-export default LoginPopup
+          <div className="password-field">
+            <input
+              name="password"
+              onChange={onChangeHandler}
+              value={data.password}
+              type={showPassword ? "text" : "password"}
+              placeholder="Enter Password"
+              required
+            />
+            <span
+              className="eye-icon"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? "👁‍🗨" : "👁️"}
+            </span>
+          </div>
+        </div>
+        <button>{currState === "Login" ? "Login" : "Create account"}</button>
+        <div className="login-popup-condition">
+          <input type="checkbox" id="terms-checkbox" required />
+          <label htmlFor="terms-checkbox" className="terms-label">
+            By continuing, I agree to the terms of use & privacy policy.
+          </label>
+        </div>
+
+        {currState === "Login" ? (
+          <p>
+            Create a new account?{" "}
+            <span onClick={() => setCurrState("Sign Up")}>Click here</span>
+          </p>
+        ) : (
+          <p>
+            Already have an account?{" "}
+            <span onClick={() => setCurrState("Login")}>Login here</span>
+          </p>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default LoginPopup;
