83f25e1d6e8527fe1464bb4b9471de67836eb499
tejhande <tejashande143143@gmail.com>
2025-04-12 16:20:22 +0530

    q

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -7,6 +7,7 @@ import List from './pages/List/List'
 import Orders from './pages/Orders/Orders'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import NotFound from '../frontend/src/pages/NotFound/NotFound';
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
           <Route path="/add" element={<Add />} />
           <Route path="/list" element={<List />} />
           <Route path="/orders" element={<Orders />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </div>
