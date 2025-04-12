d85c33e8fa7878af0f57f91422b2b625136f2f42
tejhande <tejashande143143@gmail.com>
2025-04-13 01:21:21 +0530

    `feat: updated configuration files for local development`
    * `admin/vite.config.js`: updated server host to "0.0.0.0"
    * `backend/.env`: added new environment variables for Stripe secret key, MongoDB URI, and

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+// for local testing 7-11 and new 14-20
+
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: { port: 5174 },
+// })
+
+
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5174 },
+  server: {
+    host: "0.0.0.0", // <- Add this
+    port: 5174
+  },
 })
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+// for local testing 7-10 and new 12-18
+// export default defineConfig({
+//   plugins: [react()],
+//   server: { port: 5173 },
+// })
+
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
+  server: {
+    host: "0.0.0.0", // <- Add this
+    port: 5173
+  },
 })
