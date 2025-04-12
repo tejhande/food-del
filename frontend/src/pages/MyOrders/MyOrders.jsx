3a2370607a1a3b1a75eaca7ef3033c8cc77ea1cb
tejhande <tejashande143143@gmail.com>
2025-04-12 14:39:28 +0530

    `Added CSS styles for MyOrders page and updated JSX code to reflect new styles`

--- frontend/src/pages/MyOrders/MyOrders.jsx
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -1,52 +1,72 @@
-import React, { useContext, useEffect, useState } from 'react'
-import './MyOrders.css'
-import axios from 'axios'
-import { StoreContext } from '../../Context/StoreContext';
-import { assets } from '../../assets/assets';
+import React, { useContext, useEffect, useState } from "react";
+import "./MyOrders.css";
+import axios from "axios";
+import { StoreContext } from "../../Context/StoreContext";
+import { assets } from "../../assets/assets";
 
 const MyOrders = () => {
-  
-  const [data,setData] =  useState([]);
-  const {url,token,currency} = useContext(StoreContext);
+  const [data, setData] = useState([]);
+  const { url, token, currency } = useContext(StoreContext);
 
   const fetchOrders = async () => {
-    const response = await axios.post(url+"/api/order/userorders",{},{headers:{token}});
-    setData(response.data.data)
-  }
+    const response = await axios.post(
+      url + "/api/order/userorders",
+      {},
+      { headers: { token } }
+    );
+    setData(response.data.data);
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     if (token) {
       fetchOrders();
     }
-  },[token])
+  }, [token]);
 
   return (
-    <div className='my-orders'>
+    <div className="my-orders">
       <h2>My Orders</h2>
       <div className="container">
-        {data.map((order,index)=>{
+        {[...data].reverse().map((order, index) => {
           return (
-            <div key={index} className='my-orders-order'>
-                <img src={assets.parcel_icon} alt="" />
-                <p>{order.items.map((item,index)=>{
-                  if (index === order.items.length-1) {
-                    return item.name+" x "+item.quantity
+            <div key={index} className="my-orders-order">
+              style=
+              {{
+                backgroundImage: `url(${url}/images/${order.items[0].image})`, // First item's image as background
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                height: "250px", // Fixed height for each order box
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "flex-end",
+                padding: "10px",
+                borderRadius: "8px",
+                color: "white",
+              }}
+              <p>
+                {order.items.map((item, index) => {
+                  if (index === order.items.length - 1) {
+                    return item.name + " x " + item.quantity;
+                  } else {
+                    return item.name + " x " + item.quantity + ", ";
                   }
-                  else{
-                    return item.name+" x "+item.quantity+", "
-                  }
-                  
-                })}</p>
-                <p>{currency}{order.amount}.00</p>
-                <p>Items: {order.items.length}</p>
-                <p><span>&#x25cf;</span> <b>{order.status}</b></p>
-                <button onClick={fetchOrders}>Track Order</button>
+                })}
+              </p>
+              <p>
+                {currency}
+                {order.amount}.00
+              </p>
+              <p>Items: {order.items.length}</p>
+              <p>
+                <span>&#x25cf;</span> <b>{order.status}</b>
+              </p>
+              <button onClick={fetchOrders}>Track Order</button>
             </div>
-          )
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MyOrders
+export default MyOrders;
