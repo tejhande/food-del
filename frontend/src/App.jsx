f9d54fadac1be4dd6a61e2c548b9972e98c688d7
tejhande <tejashande143143@gmail.com>
2025-04-12 14:32:23 +0530

    `Added NotFound page and route to App component`

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import MyOrders from './pages/MyOrders/MyOrders'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Verify from './pages/Verify/Verify'
+import NotFound from './pages/NotFound/NotFound';
 
 const App = () => {
 
@@ -27,6 +28,7 @@ const App = () => {
           <Route path='/order' element={<PlaceOrder />}/>
           <Route path='/myorders' element={<MyOrders />}/>
           <Route path='/verify' element={<Verify />}/>
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
       <Footer />
